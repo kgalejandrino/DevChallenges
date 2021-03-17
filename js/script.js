e9e0ac0a0566e0590b8624c4bebd20009fccc8c5
kgalejandrino <no-reply>
2021-03-16 22:16:35 -0700

    Added script file
    Added click event to increment/decrement the number of items

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* VARIABLES */
+let itemNo1 = 1;
+let itemNo2 = 1;
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+const increment_btn = document.querySelectorAll('.btn-inc');
+const decrement_btn = document.querySelectorAll('.btn-dec');
+
+/* Event: Increment Item No. */
+increment_btn.forEach(btn => {
+    btn.addEventListener('click', (evt) => {
+        if(btn.parentNode.id === 'js--value1') {
+            itemNo1++;
+            input1.value = itemNo1;
+        } else if(btn.parentNode.id === 'js--value2') {
+            itemNo2++;
+            input2.value = itemNo2;
+        }
+    })
+});
+
+/* Event: Decrement Item No. */
+decrement_btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(btn.parentNode.id === 'js--value1' && input1.value > 1) {
+            itemNo1--;
+            input1.value = itemNo1;
+        } else if(btn.parentNode.id === 'js--value2' && input2.value > 1) {
+            itemNo2--;
+            input2.value = itemNo2;
+        }
+    })
+});
+
