8d1cf4c76a80d52ed8c796941dc461f66d42f2fa
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-03-17 17:20:32 -0700

    Added HTML5 input required
    Added submit function form with an alert popup
    
    Fixed mobile query on input overflow

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 /* VARIABLES */
 let itemNo1 = 1;
 let itemNo2 = 1;
-let input1 = document.querySelector('#input1');
-let input2 = document.querySelector('#input2');
+let item1_value = document.querySelector('#item1_value');
+let item2_value = document.querySelector('#item2_value');
 const increment_btn = document.querySelectorAll('.btn-inc');
 const decrement_btn = document.querySelectorAll('.btn-dec');
+const inputs = document.getElementsByTagName('input');
+const select = document.querySelector('select');
+const form = document.querySelector('form');
+
 
 /* Click Event: Increment Item No. */
 increment_btn.forEach(btn => {
-    btn.addEventListener('click', (evt) => {
+    btn.addEventListener('click', () => {
         if(btn.parentNode.id === 'js--value1') {
             itemNo1++;
-            input1.value = itemNo1;
+            item1_value.value = itemNo1;
         } else if(btn.parentNode.id === 'js--value2') {
             itemNo2++;
-            input2.value = itemNo2;
+            item2_value.value = itemNo2;
         }
     })
 });
@@ -22,12 +26,12 @@ increment_btn.forEach(btn => {
 /* Click Event: Decrement Item No. */
 decrement_btn.forEach(btn => {
     btn.addEventListener('click', () => {
-        if(btn.parentNode.id === 'js--value1' && input1.value > 1) {
+        if(btn.parentNode.id === 'js--value1' && item1_value.value > 1) {
             itemNo1--;
-            input1.value = itemNo1;
-        } else if(btn.parentNode.id === 'js--value2' && input2.value > 1) {
+            item1_value.value = itemNo1;
+        } else if(btn.parentNode.id === 'js--value2' && item2_value.value > 1) {
             itemNo2--;
-            input2.value = itemNo2;
+            item2_value.value = itemNo2;
         }
     })
 });
@@ -42,6 +46,24 @@ document.querySelectorAll('input[type="number"]').forEach(input => {
     })
 });
 
+/* Function: Check Validation */
+const checkInput = (evt) => {
+    for(let i = 0; i < inputs.length; i++) {
+        if(inputs[i].type !== 'number' && inputs[i].type !== 'checkbox'){
+            console.log(inputs[i]);
+        }
+    }
+}
+
+/* Submit Event: Continue Confirmation */
+form.addEventListener('submit', e => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        alert('Purchase complete. Thank you for your order.');
+    }
+    e.preventDefault();
+    alert('Purchase complete. Thank you for your order.');
+})
 
 
 
