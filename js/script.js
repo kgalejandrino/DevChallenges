9fc78cbfa0f6a8e2cd61d595ab704d74645715a5
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-03-20 15:45:24 -0700

    Added fix navigation for mobile
    Added navbar functionality when hamburger menu is clicked

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ const servicesSection = document.querySelector('.services-section');
 const worksSection = document.querySelector('.works-section');
 const teamSection = document.querySelector('.team-section');
 const footer = document.querySelector('#contact');
+const hamburgerMenu = document.querySelector('#hamburger-menu')
+const mainNav = document.querySelector('.nav-ul');
 
+console.log(mainNav);
 
 /* FUNCTION: Smooth Scrolling */
 const smoothScroll = (section) => {
@@ -37,3 +40,24 @@ btnScrollTo.forEach(links => {
         }
     });
 });
+
+
+/* FUNCTION: TOGGLE MENU */
+// Note: With material icon, the textual name inside the span
+// is the icon being rendered. Hence, in this function we are
+// checking the text content and toggle the value between menu 
+// (being open) & close (being close) 
+const toggleHamburger = () => {
+    if(hamburgerMenu.textContent === 'menu') {
+        hamburgerMenu.textContent = 'close';
+        mainNav.classList.remove('nav-ul');
+        mainNav.classList.add('js--mobile-ul');
+    } else {
+        hamburgerMenu.textContent = 'menu';
+        mainNav.classList.remove('js--mobile-ul');
+        mainNav.classList.add('nav-ul');
+    }
+}
+
+/* Click Event: Display Hamburger menu */
+hamburgerMenu.addEventListener('click', toggleHamburger);
