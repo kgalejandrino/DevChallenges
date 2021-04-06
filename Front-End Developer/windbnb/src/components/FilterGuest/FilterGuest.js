1a06c618ce9799747573fac3622ae61a625de583
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-06 16:53:27 -0700

    Added a functionality to increment/decrement the adult guest value

--- Front-End Developer/windbnb/src/components/FilterGuest/FilterGuest.js
+++ Front-End Developer/windbnb/src/components/FilterGuest/FilterGuest.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './FilterGuest.css';
 import Guest from './Guest/Guest';
 
-const filterGuest = (props) => {
+const FilterGuest = (props) => {
+    let [adultValue, setAdultValue] = useState(0);
+    let [childrenValue, setChildrenValue] = useState(0);
+
+    const changeAdultValue = (event) => {
+        console.log('test');
+        if(event.target.innerText === '+' && adultValue < 99) {
+            setAdultValue(adultValue + 1);
+        } else if(event.target.innerText === '-' && adultValue > 0) {
+            setAdultValue(adultValue - 1);
+        }
+    }
+
     return (
         <div className="FilterGuest">
             <Guest 
                 category="Adults"
-                age="Ages 13 or above"
-            />
+                age="Ages 13 or above">
+                <div className="inc-dec--btn">
+                    <button className="btn btn-dec" onClick={changeAdultValue}>-</button>
+                    <span className="value">{adultValue}</span>
+                    <button className="btn btn-inc" onClick={changeAdultValue}>+</button>
+                </div>
+            </Guest>
             <Guest 
                 category="Children"
-                age="Ages 2-12"
-            />
+                age="Ages 2-12">
+                <div className="inc-dec--btn">
+                    <button className="btn btn-dec">-</button>
+                    <span className="value">{childrenValue}</span>
+                    <button className="btn btn-inc">+</button>
+                </div>
+            </Guest>
         </div>
     )
 }
 
-export default filterGuest;
+export default FilterGuest;
--- Front-End Developer/windbnb/src/components/FilterGuest/Guest/Guest.js
+++ Front-End Developer/windbnb/src/components/FilterGuest/Guest/Guest.js
@@ -7,11 +7,7 @@ const guest = props => {
         <div className="guest">
             <p className="category">{props.category}</p>
             <p className="age">Ages 13 or above</p>
-            <div className="inc-dec--btn">
-                <button className="btn btn-dec">-</button>
-                <span className="value">0</span>
-                <button className="btn btn-inc">+</button>
-            </div>
+            {props.children}
         </div>        
     )
 }
