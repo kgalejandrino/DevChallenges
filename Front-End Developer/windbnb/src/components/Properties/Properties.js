9f23efd1714a2b8a83f6f139e587f049bf4feddd
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-07 20:34:02 -0700

    Dynamically change the title 'Stays in (location)that the user has selected

--- Front-End Developer/windbnb/src/components/Properties/Properties.js
+++ Front-End Developer/windbnb/src/components/Properties/Properties.js
@@ -5,26 +5,30 @@ import Aux from '../../hoc/Auxilliary/Auxilliary';
 import Property from './Property/Property';
 
 const properties = (props) => {
-    
+    const properties = props.propertiesList.filter(key => key.location === props.location);
+    let cardStyle = '';
+
+    if(properties.length < 3) {
+        cardStyle = { justifyContent: 'flex-start' }
+    }
+
     return (
         <Aux>
             <div className="Properties">
                 <div className="title-box">
-                    <h2>Stays in Finland</h2>
+                    <h2>Stays in {props.location ? props.location : 'Finland'}</h2>
                     <p>12+ stays</p>
                 </div>
-                <div className="properties-box">
-                    {props.propertiesList.map((key, index) => {
-                        if((key.location === props.location)) {
-                            return <Property 
-                            key={index}
-                            url={key.url}
-                            host={key.host}
-                            type={key.type}
-                            rating={key.rating}
-                            description={key.description}
-                        />
-                        }
+                <div className="properties-box" style={cardStyle}>
+                    {properties.map((key, index) => {
+                        return <Property 
+                                    key={index}
+                                    url={key.url}
+                                    host={key.host}
+                                    type={key.type}
+                                    rating={key.rating}
+                                    description={key.description}
+                                />
                     })}
                 </div>
             </div>
