dab053e7793c3b6e007c3ce81041d807adfa06d3
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-10 15:45:26 -0700

    Project setup: Todo App

---
+++ Front-End Developer/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+// font-family: 'Montserrat', sans-serif;
+// font-family: 'Raleway', sans-serif;
