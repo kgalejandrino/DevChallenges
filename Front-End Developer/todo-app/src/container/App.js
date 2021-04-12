e7be0ba0f0e1bdca84c79480f564139d29fb2192
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-12 14:07:59 -0700

    Added hover style on tabs

--- Front-End Developer/todo-app/src/container/App.js
+++ Front-End Developer/todo-app/src/container/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1>#todo</h1>
           <Tabs />
           <AddTask />
-          <TaskList />
+          {/* <TaskList /> */}
       </div>
     );
   }
