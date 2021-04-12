df846c3200f100111514130cb1728c6127906656
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-12 16:17:48 -0700

    Added a strikethrough styling for task that is mark completed by the user

--- Front-End Developer/todo-app/src/components/TaskList/TaskList.js
+++ Front-End Developer/todo-app/src/components/TaskList/TaskList.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './TaskList.css';
 
-const taskList = (props) => {
+const TaskList = (props) => {
+    let [check, setCheck] = useState(false);
+    const handleCheckboxClick = () => setCheck(true);
+
+    const strikethrough = { 
+        textDecoration: "line-through",
+        color: "#333"
+    }
+
     return (
         <div className="TaskList">
-            <input type="checkbox" id="task" className="list-checkbox"></input>
-            <label htmlFor="task" className="label-checkbox">{props.children}</label>
+            <input 
+                type="checkbox" 
+                id="task" 
+                className="list-checkbox"
+                onClick={handleCheckboxClick}>
+            </input>
+            <label 
+                htmlFor="task" 
+                className="label-checkbox"
+                style={check ? strikethrough : null}>{props.children}
+            </label>
         </div>
     )
 }
 
-export default taskList;
+export default TaskList;
