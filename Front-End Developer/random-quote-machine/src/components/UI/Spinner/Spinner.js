8feb424d716afb7796717542e020fd6ed594a7af
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-17 12:51:28 -0700

    Added loading spinner while waiting for data to be fetched from the server

---
+++ Front-End Developer/random-quote-machine/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import './Spinner.css';
+
+const spinner = () => {
+    return (
+        <div className="Spinner">
+            <div class="loader">Loading...</div>
+        </div>
+    )
+}
+
+export default spinner;
--- Front-End Developer/random-quote-machine/src/container/App.js
+++ Front-End Developer/random-quote-machine/src/container/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 
 import './App.css';
 import axios from 'axios';
+import Aux from '../hoc/Auxilliary';
 import Quote from '../components/Quote/Quote';
 import Author from '../components/Author/Author';
+import Spinner from '../components/UI/Spinner/Spinner';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
       this.state = {
-        author: '',
-        genre: '',
-        text: ''
+        data: [],
+        loading: false
       }
     }
 
@@ -21,14 +22,14 @@ class App extends Component {
     }
 
     fetchRandomQuote = () => {
+      this.setState({ loading: true })
       axios.get(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
       .then(response => {
         console.log(response.data.data[0]);
         const data = response.data.data[0];
-        this.setState({
-          author: data.quoteAuthor,
-          genre: data.quoteGenre,
-          text: data.quoteText
+        this.setState({ 
+          data: data,
+          loading: false 
         })
       })  
       .catch(error => {
@@ -37,19 +38,29 @@ class App extends Component {
     }
 
   render() {
+    let { data } = this.state;
+    let quote = 
+      (
+        <Aux>
+          <Quote 
+            text={data.quoteText}
+          />
+          <Author 
+            author={data.quoteAuthor}
+            genre={data.quoteGenre}
+          />
+        </Aux>
+      )
+    
+    if(this.state.loading) { quote = <Spinner /> }
+    
     return (
       <div className="App">
         <div className="random-container">
           <span className="random-text">random</span>
           <span className="material-icons icon-random" onClick={this.fetchRandomQuote}>autorenew</span>
         </div>
-        <Quote 
-          text={this.state.text}
-        />
-        <Author 
-          author={this.state.author}
-          genre={this.state.genre}
-        />
+          { quote }
       </div>
     )
   }
