82d9ab80750bd2ba2cb35b09a029b9242fbf88d9
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-04-16 23:07:53 -0700

    Added functionality that would fetch random quote from the server by clicking the random icon

--- Front-End Developer/random-quote-machine/src/container/App.js
+++ Front-End Developer/random-quote-machine/src/container/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 import './App.css';
 import axios from 'axios';
-import Quote from './components/Quote/Quote';
-import Author from './components/Author/Author';
+import Quote from '../components/Quote/Quote';
+import Author from '../components/Author/Author';
 
 class App extends Component {
   constructor(props) {
@@ -17,28 +17,31 @@ class App extends Component {
     }
 
     componentDidMount() {
+      this.fetchRandomQuote();
+    }
+
+    fetchRandomQuote = () => {
       axios.get(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
-        .then(response => {
-          console.log(response.data.data[0]);
-          const data = response.data.data[0];
-          this.setState({
-            author: data.quoteAuthor,
-            genre: data.quoteGenre,
-            text: data.quoteText
-          })
-        })  
-        .catch(error => {
-          console.log(error);
+      .then(response => {
+        console.log(response.data.data[0]);
+        const data = response.data.data[0];
+        this.setState({
+          author: data.quoteAuthor,
+          genre: data.quoteGenre,
+          text: data.quoteText
         })
+      })  
+      .catch(error => {
+        console.log(error);
+      })
     }
 
   render() {
-    console.log(this.state.text);
     return (
       <div className="App">
         <div className="random-container">
-          <span>random</span>
-          <span className="material-icons">autorenew</span>
+          <span className="random-text">random</span>
+          <span className="material-icons icon-random" onClick={this.fetchRandomQuote}>autorenew</span>
         </div>
         <Quote 
           text={this.state.text}
