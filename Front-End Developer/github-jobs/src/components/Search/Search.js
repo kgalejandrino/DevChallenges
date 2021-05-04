6153e9a216ce9577cabc5a31831e330d2beb574e
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-05-03 20:00:25 -0700

    Added all cosearch component's content and styling

--- Front-End Developer/github-jobs/src/components/Search/Search.js
+++ Front-End Developer/github-jobs/src/components/Search/Search.js
@@ -5,7 +5,15 @@ import './Search.css';
 const search = () => {
     return (
         <div className="Search">
-            
+            <div className="search-container">
+                <div className="input-container">
+                    <span class="material-icons icon-search">work_outline</span>
+                    <input type="text" className="input" placeholder="Title, companies, expertise or benefits"></input>
+                </div>
+                <div className="btn-container">
+                    <button className="btn btn-search">Search</button>
+                </div>
+            </div>
         </div>
     )
 }
