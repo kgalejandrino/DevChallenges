685064101ea2b82728a546cc6b91ca0273242f27
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-05-03 21:08:23 -0700

    Added filtersearch components content and styling

--- Front-End Developer/github-jobs/src/components/Main/FilterSearch/FilterSearch.js
+++ Front-End Developer/github-jobs/src/components/Main/FilterSearch/FilterSearch.js
@@ -3,9 +3,27 @@ import React from 'react';
 import './FilterSearch.css';
 
 const filterSearch = () => {
+    const country = ['London', 'Amsterdam', 'New York', 'Berlin'];
+
     return(
         <div className="FilterSearch">
-
+            <div className="checkbox_container">
+                <input type="checkbox" name="fulltime" className="checkbox"></input>
+                <label for="fulltime" className="checkbox-label">Full time</label>
+            </div>
+            <div className="location_container">
+                <p className="location-title">Location</p>
+                <div className="loc-input_container">
+                    <span class="material-icons icon-location">public</span>
+                    <input type="text" placeholder="City, state, zip or country" className="input-location"></input>
+                </div>
+            </div>
+            {country.map((country, index) => {
+                return <div className="options-container" key={index}>
+                            <input type="radio" name="{country}" className="option"></input>
+                            <label for="{country}" className="option-label">{country}</label>
+                        </div>
+            })}
         </div>
     )
 }
