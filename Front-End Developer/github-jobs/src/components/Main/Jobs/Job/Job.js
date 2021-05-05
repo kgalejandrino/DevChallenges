522a4d27a47bdf1feaabb6a44e38b060e69232e0
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-05-04 22:27:57 -0700

    Added job components that contains the list of jobs with their company logo, name, location & posted time. Also added the styling of the component

---
+++ Front-End Developer/github-jobs/src/components/Main/Jobs/Job/Job.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './Job.css';
+
+const job = () => {
+    return(
+        <div className="Job">
+            <div className="job-left--box">
+                <div className="logo_container">
+                    <img src="https://github-jobs.s3.amazonaws.com/okF4QAWNFFWEUanmBAMdCq4S?response-content-disposition=inline%3B%20filename%3D%22bluevine-leaf-rgb.png%22%3B%20filename%2A%3DUTF-8%27%27bluevine-leaf-rgb.png&response-content-type=image%2Fpng&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJENXOYUQN2IQEWRA%2F20210505%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210505T030335Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=1208d96be4503c5cd91b5ece442f051c6f42bba03db745e592c64c438d3b346f" alt="logo"></img>
+                </div>
+                <div className="info_container">
+                    <p className="company-name">Kasisto</p>
+                    <p className="job-title">Front-End Software Engineer</p>
+                    <p className="job-type">Full Time</p>
+                </div>
+            </div>
+            <div className="job-right--box">
+                <div className="city_container">
+                    <span class="material-icons city-icon">public</span>
+                    <span>New York</span>
+                </div>
+                <div className="time_container">
+                    <span class="material-icons time-icon">schedule</span>
+                    <span>5 days ago</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default job;
--- Front-End Developer/github-jobs/src/components/Main/Jobs/Jobs.js
+++ Front-End Developer/github-jobs/src/components/Main/Jobs/Jobs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './Jobs.css';
+import Job from './Job/Job';
 
 const jobs = () => {
     return(
         <div className="Jobs">
-
+            <Job />
         </div>
     )
 }
